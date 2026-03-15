abdbc799413e0f228a2b5e52dd6cb0bdb4cf3b5c
Koyena06 <koyena.sutradhar@gmail.com>
2026-03-15 20:37:33 +0530

    header and footer added

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <p>Converge - Meetup App &#169; 2026</p>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php
+if(session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+?>
+
+<div class="navbar">
+
+    <div class="logo">
+        <a href="/meetup-app/index.php">Meetup</a>
+    </div>
+
+    <div class="nav-links">
+
+        <?php if(isset($_SESSION["user_id"])) { ?>
+
+            <a href="/meetup-app/user/dashboard.php">Dashboard</a>
+            <a href="/meetup-app/user/profile.php">Profile</a>
+            <a href="/meetup-app/auth/logout.php">Logout</a>
+
+        <?php } else { ?>
+
+            <a href="/meetup-app/auth/login.php">Login</a>
+            <a href="/meetup-app/auth/register.php">Register</a>
+
+        <?php } ?>
+
+    </div>
+
+</div>
