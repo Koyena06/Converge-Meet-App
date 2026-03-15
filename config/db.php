61ebc482d1c3aef9dfc6770c5143049990eef7b7
Koyena <koyena.sutradhar@gmail.com>
2026-03-15 16:38:46 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
     $host="localhost";
     $user="root";
     $password="";
-    $database="meetup_app";
+    $database="meetup_db";
 
     //create connection 
     $conn=new mysqli($host, $user, $password, $database);
