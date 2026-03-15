a9d0fbc2b4211d23c77819b8f4ab964be4c5a286
Koyena <koyena.sutradhar@gmail.com>
2026-03-15 16:30:52 +0530

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -15,7 +15,7 @@
                  $_SESSION["user_id"]=$user["id"];
                  $_SESSION["name"]=$user["name"];
 
-                 header("Location: ../user/dashboarsd.php");
+                 header("Location: ../user/dashboard.php");
             }
             else{
                 echo "Incorrect Password";
